54ac1ad969939da83967235ec66f666676f256cb
DavitBakh <davobakhshyan@gmail.com>
2024-10-05 18:02:15 +0400

    Test 1

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -2,8 +2,38 @@
 // given in main.cpp.
 
 // Again: Declare your functions inside your class, but define them outside of it.
-
-#include <vector>
+#include <cstddef>
 
 template <typename T>
-using Vector = std::vector<T>;
+class Vector
+{
+private:
+	size_t _size;
+	size_t _capacity;
+	T* _arr;
+
+public:
+	Vector(size_t size = 0, T defaultValue = 0);
+	size_t size() const;
+	~Vector();
+
+};
+
+template <typename T>
+Vector<T>::Vector(size_t size, T defaultValue) : _size(size), _capacity(size), _arr(new T[size]) 
+{
+	for (size_t i = 0; i < _size; i++)
+		_arr[i] = defaultValue;
+}
+
+template <typename T>
+size_t Vector<T>::size() const
+{
+	return _size;
+}
+
+template <typename T>
+Vector<T>::~Vector()
+{
+	delete[] _arr;
+}
